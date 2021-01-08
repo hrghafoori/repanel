b4147e5df97da1f9d93210067e8f215dc5a7e96b
hrghafouri <public.ghafouri@gmail.com>
2021-01-08 19:55:16 +0330

    SdfsDf

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,10 +14,30 @@ function App() {
         <div className='sideContainer'>
             <div className='sideChild'>
                 <SidebarBtn
-                    to='http://google.com'
-                    tar='_blank'
+                    // to='http://google.com'
+                    active
+                    tree
                     icon={faTachometerAlt}
-                    show='داشبورد'/>
+                    show='داشبورد'
+                    treeContent={[
+                        ['لینک اول', 'http://me.ir'],
+                        ['وسط لینک الان', 'http://he.ir'],
+                        ['دوباره اخری', 'http://you.ir']
+                    ]}/>
+                {/*<SidebarBtn*/}
+                {/*    to='http://google.com'*/}
+                {/*    tree*/}
+                {/*    icon={faTachometerAlt}*/}
+                {/*    show='داشبورد'*/}
+                {/*    treeContent={[*/}
+                {/*        ['لینک اول', 'http://me.ir'],*/}
+                {/*        ['وسط لینک الان', 'http://he.ir'],*/}
+                {/*        ['دوباره اخری', 'http://you.ir']*/}
+                {/*    ]}/>*/}
+                {/*<SidebarBtn*/}
+                {/*    to='http://google.com'*/}
+                {/*    icon={faTachometerAlt}*/}
+                {/*    show='داشبورد'/>*/}
             </div>
         </div>
     );
--- src/components/SidebarBtn.js
+++ src/components/SidebarBtn.js
@@ -1,19 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/Main.css'
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-
-
+import {faChevronLeft, faCircleNotch} from '@fortawesome/free-solid-svg-icons'
+import $ from 'jquery'
 
 function App(props) {
+    const [TC, setTC] = useState('');
+    let treeContent = [];
+    let active = 'sideBtnParent ';
+    if (props.active)
+        active = 'sideBtnParent  ForceActive';
+    let tree = '';
+    if (props.tree)
+        tree = <FontAwesomeIcon icon={faChevronLeft}/>;
     useEffect(() => {
-
+        $(document).on('click', '.ForceActive', function (el) {
+            $(this).toggleClass('open');
+            $(this).find('ul').slideToggle(100);
+        });
+        if (props.tree)
+            for (let i = 0; i < props.treeContent.length; i++) {
+                treeContent.push(<li key={i}><FontAwesomeIcon icon={faCircleNotch}/><a
+                    href={props.treeContent[i][1]}>{props.treeContent[i][0]}</a></li>)
+                setTC(treeContent);
+            }
     }, []);
 
     return (
-        <a target={props.tar} href={props.to} className='sideBtnParent'>
+        <a href={props.to} className={active}>
             <FontAwesomeIcon icon={props.icon}/>
             <span>{props.show}</span>
+            {tree}
+            {(props.tree) ? (<ul>{TC}</ul>) : null}
         </a>
     );
 }
